c83d473dc90c25b0a25fe4d875283d2fb7beea15
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-14 18:50:34 -0300

    Configurando o Hilt para testes de integração com Espresso

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -2,12 +2,19 @@ package com.example.marvelapp.presentation.characters
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class CharactersFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Before
     fun setUp() {
@@ -16,6 +23,6 @@ class CharactersFragmentTest {
 
     @Test
     fun shouldShowCharacters_whenViewIsCreated() {
-        
+
     }
 }
