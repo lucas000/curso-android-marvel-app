4c8e4952540792a1ea4d639e12860ccefdf0bc12
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-16 09:31:50 -0300

    Criando View Model e Fragmento de detalhes do personagem

---
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
@@ -0,0 +1,29 @@
+package com.example.marvelapp.presentation.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.marvelapp.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentDetailBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
