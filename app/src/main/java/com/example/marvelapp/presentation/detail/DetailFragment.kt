122d8963d44801e9c001305abd82451f0db336e9
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-16 14:41:09 -0300

    Implementando o adaptador do item child do Nested RecyclerView

--- app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
@@ -1,7 +1,6 @@
 package com.example.marvelapp.presentation.detail
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -39,7 +38,6 @@ class DetailFragment : Fragment() {
         _binding = this
     }.root
 
-    @Suppress("MagicNumber")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -53,12 +51,13 @@ class DetailFragment : Fragment() {
 
         viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
             val logResult = when (uiState) {
-                DetailViewModel.UiState.Loading -> "Loading comics..."
-                is DetailViewModel.UiState.Success -> uiState.comics.toString()
-                DetailViewModel.UiState.Error -> "Error when loading"
+                DetailViewModel.UiState.Loading -> {
+                }
+                is DetailViewModel.UiState.Success -> {
+                }
+                DetailViewModel.UiState.Error -> {
+                }
             }
-
-            Log.d(DetailFragment::class.simpleName, logResult)
         }
 
         viewModel.getComics(detailViewArg.characterId)
