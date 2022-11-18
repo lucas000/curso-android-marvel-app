5ee3d6f505ec0bf37d284aca9308cb772a740fa3
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-18 10:29:40 -0300

    Criando a entidade FavoriteEntity

---
+++ core/src/main/java/com/example/core/data/DbConstants.kt
@@ -0,0 +1,9 @@
+package com.example.core.data
+
+object DbConstants {
+
+    const val FAVORITES_TABLE_NAME = "favorites"
+    const val FAVORITES_COLUMN_INFO_ID = "id"
+    const val FAVORITES_COLUMN_INFO_NAME = "name"
+    const val FAVORITES_COLUMN_INFO_IMAGE = "image_url"
+}
