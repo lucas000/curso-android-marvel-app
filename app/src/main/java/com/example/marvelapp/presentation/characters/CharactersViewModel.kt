725e370cb72026627ccede01f468754205a186e4
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-18 17:03:55 -0300

    Mapeando ações e estados de UI no view model de personagens

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersViewModel.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersViewModel.kt
@@ -1,20 +1,38 @@
 package com.example.marvelapp.presentation.characters
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.core.usecase.GetCharactersUseCase
 import com.example.core.domain.model.Character
+import com.example.core.usecase.base.CoroutinesDispatchers
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersViewModel @Inject constructor(
-    private val getCharactersUseCase: GetCharactersUseCase
+    private val getCharactersUseCase: GetCharactersUseCase,
+    private val coroutinesDispatchers: CoroutinesDispatchers
 ) : ViewModel() {
+
+    private val action = MutableLiveData<Action>()
+
+    val state: LiveData<UiState> = action
+        .distinctUntilChanged()
+        .switchMap { action ->
+            when(action) {
+                is Action.Search -> {
+                    getCharactersUseCase(
+                        GetCharactersUseCase.GetCharactersParams(action.query, getPageConfig())
+                    ).cachedIn(viewModelScope).map {
+                        UiState.SearchResult(it)
+                    }.asLiveData(coroutinesDispatchers.main())
+                }
+            }
+        }
 
     fun charactersPagingData(query: String): Flow<PagingData<Character>> {
         return getCharactersUseCase.invoke(
@@ -25,4 +43,16 @@ class CharactersViewModel @Inject constructor(
     private fun getPageConfig() = PagingConfig(
         pageSize = 20
     )
+
+    fun searchCharacters(query: String = "") {
+        action.value = Action.Search(query)
+    }
+
+    sealed class UiState {
+        data class SearchResult(val data: PagingData<Character>) : UiState()
+    }
+
+    sealed class Action {
+        data class Search(val query: String) : Action()
+    }
 }
