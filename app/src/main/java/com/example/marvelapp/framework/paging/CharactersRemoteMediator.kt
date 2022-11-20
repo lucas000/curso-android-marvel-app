5376dea1e551493e5692a4c903af16ca9cb75bf0
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-19 22:25:01 -0300

    Implementando o RemoteMediator - Parte 2

--- app/src/main/java/com/example/marvelapp/framework/paging/CharactersRemoteMediator.kt
+++ app/src/main/java/com/example/marvelapp/framework/paging/CharactersRemoteMediator.kt
@@ -7,6 +7,8 @@ import androidx.paging.RemoteMediator
 import com.example.core.data.repository.CharactersRemoteDataSource
 import com.example.marvelapp.framework.db.AppDatabase
 import com.example.marvelapp.framework.db.entity.CharacterEntity
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 @OptIn(ExperimentalPagingApi::class)
@@ -16,10 +18,19 @@ class CharactersRemoteMediator @Inject constructor(
     private val remoteDataSource: CharactersRemoteDataSource
 ) : RemoteMediator<Int, CharacterEntity>() {
 
+    private val characterDao = database.characterDao()
+    private val remoteKeyDao = database.remoteKeyDao()
+
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, CharacterEntity>
     ): MediatorResult {
-        TODO("Not yet implemented")
+        return try {
+            MediatorResult.Success(endOfPaginationReached = true)
+        } catch (e: IOException) {
+            MediatorResult.Error(e)
+        } catch (e: HttpException) {
+            MediatorResult.Error(e)
+        }
     }
 }
