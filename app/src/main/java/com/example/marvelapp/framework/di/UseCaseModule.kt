d44a7438b64b065ef0db8a83a211ebe6ecabc1cb
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-20 10:26:44 -0300

    Implementando uma nova função de mapeamento na classe SortingMapper

--- app/src/main/java/com/example/marvelapp/framework/di/UseCaseModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/UseCaseModule.kt
@@ -14,6 +14,8 @@ import com.example.core.usecase.GetFavoritesUseCase
 import com.example.core.usecase.GetFavoritesUseCaseUseCaseImpl
 import com.example.core.usecase.RemoveFavoriteUseCase
 import com.example.core.usecase.RemoveFavoriteUseCaseImpl
+import com.example.core.usecase.SaveCharactersSortingUseCase
+import com.example.core.usecase.SaveCharactersSortingUseCaseImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -48,4 +50,8 @@ interface UseCaseModule {
         useCase: GetCharactersSortingUseCaseImpl
     ): GetCharactersSortingUseCase
 
+    @Binds
+    fun bindSaveCharactersSortingUseCase(
+        useCase: SaveCharactersSortingUseCaseImpl
+    ): SaveCharactersSortingUseCase
 }
