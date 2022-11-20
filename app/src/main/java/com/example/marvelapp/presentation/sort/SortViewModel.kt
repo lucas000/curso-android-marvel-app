05763709c72b0c4a888cddf1b3fe6d33afae102a
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-20 10:31:40 -0300

    Implementando a classe SortViewModel - Parte 1

--- app/src/main/java/com/example/marvelapp/presentation/sort/SortViewModel.kt
+++ app/src/main/java/com/example/marvelapp/presentation/sort/SortViewModel.kt
@@ -1,8 +1,42 @@
 package com.example.marvelapp.presentation.sort
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.core.usecase.GetCharactersSortingUseCase
+import com.example.core.usecase.SaveCharactersSortingUseCase
+import com.example.core.usecase.base.AppCoroutinesDispatchers
 import javax.inject.Inject
 
-class SortViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class SortViewModel @Inject constructor(
+    private val getCharactersSortingUseCase: GetCharactersSortingUseCase,
+    private val saveCharactersSortingUseCase: SaveCharactersSortingUseCase,
+    private val coroutinesDispatchers: AppCoroutinesDispatchers
+) : ViewModel() {
+
+    private val action = MutableLiveData<Action>()
+
+    init {
+        action.value = Action.GetStoredSorting
+    }
+
+    fun applySorting(orderBy: String, order: String) {
+        action.value = Action.ApplySorting(orderBy, order)
+    }
+
+    sealed class UiState {
+        data class SortingResult(val storedSorting: Pair<String, String>) : UiState()
+        sealed class ApplyState : UiState() {
+            object Loading : ApplyState()
+            object Success : ApplyState()
+            object Error : ApplyState()
+        }
+    }
+
+    sealed class Action {
+        object GetStoredSorting : Action()
+        data class ApplySorting(
+            val orderBy: String,
+            val order: String
+        ) : Action()
+    }
 }
