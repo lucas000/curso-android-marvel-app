30b8de18d086e0108ad57273453cc8671b742bae
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-20 10:38:51 -0300

    Implementando a classe SortViewModel - Parte 2

--- app/src/main/java/com/example/marvelapp/presentation/sort/SortViewModel.kt
+++ app/src/main/java/com/example/marvelapp/presentation/sort/SortViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.marvelapp.presentation.sort
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
 import com.example.core.usecase.GetCharactersSortingUseCase
 import com.example.core.usecase.SaveCharactersSortingUseCase
 import com.example.core.usecase.base.AppCoroutinesDispatchers
+import com.example.marvelapp.presentation.extensions.watchStatus
 import javax.inject.Inject
 
 class SortViewModel @Inject constructor(
@@ -14,6 +18,36 @@ class SortViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val action = MutableLiveData<Action>()
+    val state: LiveData<UiState> = action.switchMap { action ->
+        liveData(coroutinesDispatchers.main()) {
+            when(action) {
+                Action.GetStoredSorting -> {
+                    getCharactersSortingUseCase.invoke()
+                        .collect { sortingPair ->
+                            emit(UiState.SortingResult(sortingPair))
+                        }
+                }
+                is Action.ApplySorting -> {
+                    val orderBy = action.orderBy
+                    val order = action.order
+
+                    saveCharactersSortingUseCase.invoke(
+                        SaveCharactersSortingUseCase.Params(orderBy to order)
+                    ).watchStatus(
+                        loading = {
+                            emit(UiState.ApplyState.Loading)
+                        },
+                        success = {
+                            emit(UiState.ApplyState.Success)
+                        },
+                        error = {
+                            emit(UiState.ApplyState.Error)
+                        }
+                    )
+                }
+            }
+        }
+    }
 
     init {
         action.value = Action.GetStoredSorting
