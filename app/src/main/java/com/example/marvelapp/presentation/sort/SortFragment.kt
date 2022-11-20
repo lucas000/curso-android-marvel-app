a95e931e8768060093731b198f5bbcbee3436945
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-19 23:23:01 -0300

    Criando uma nova feature para a ordenação

---
+++ app/src/main/java/com/example/marvelapp/presentation/sort/SortFragment.kt
@@ -0,0 +1,34 @@
+package com.example.marvelapp.presentation.sort
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.marvelapp.databinding.FragmentSortBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SortFragment : Fragment() {
+
+    private var _binding: FragmentSortBinding? = null
+    private val binding: FragmentSortBinding get() = _binding!!
+
+    private val viewModel: SortViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentSortBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/marvelapp/presentation/sort/SortViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.marvelapp.presentation.sort
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class SortViewModel @Inject constructor() : ViewModel() {
+    // TODO: Implement the ViewModel
+}
